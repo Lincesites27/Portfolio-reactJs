25767a14c851d5d6a1087ddb8bb8a890c74bd551
Lincesites27 <contato@lincesites.com.br>
2022-08-26 18:20:55 -0300

    efeito do botao

--- src/components/Sessao1.js
+++ src/components/Sessao1.js
@@ -10,6 +10,7 @@ const Sessao1 = () => {
        <h1>Lince Sites</h1>
        <h2>Sou Desenvolvedor Web a mais de 5 anos, sou especialista em E-commerce, Landing-Page, Dropshipping, Sites Institucionais, Tráfego pago, métricas de SEO, Ux/Ui, Design e muitooo mais...</h2>
        <p>Analista de sistemas em formação, tenho total conhecimento nas principais linguagens de programação: Html5, Css3, Sass, JavaScript, Php, Wordress, Banco de dados MySqL.</p>
+        <button className={styles.btn}>Já vi o que preciso e já quero!</button>
         </div> 
 
         <div className={styles.row2}>
